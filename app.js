3d3a340ff2fde0e1d64b98da488c647b8f1738e6
utkuzun <utkuzun50@gmail.com>
2022-12-12 16:30:28 +0300

    read all, create and delete endpoints added

--- app.js
+++ app.js
@@ -1,12 +1,95 @@
 require('dotenv').config()
+const { Sequelize, DataTypes, Model } = require('sequelize')
 const express = require('express')
 
 const app = express()
+app.use(express.json())
+
+const sequelize = new Sequelize(process.env.FLY_POSTGRE_URL)
 
 const PORT = process.env.PORT || 3000
 
-const main = () => {
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
+  }
+)
+
+Blog.sync()
+
+app.get('/', (req, res) => {
   try {
+    console.log('entered api')
+    return res.send('an api')
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.get('/api/blogs', async (req, res) => {
+  try {
+    const blogs = await Blog.findAll()
+
+    return res.status(200).json(blogs.map((item) => item.toJSON()))
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const newBlog = await Blog.create(req.body)
+    return res.status(201).json(newBlog.toJSON())
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  try {
+    const blog = await Blog.findByPk(req.params.id)
+    if (!blog) {
+      return res.status(404).send('Blog can not be found!!')
+    }
+
+    await blog.destroy()
+    return res.status(204).send()
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+const main = async () => {
+  try {
+    await sequelize.authenticate()
+    console.log('Connection establihed!!')
     app.listen(PORT, () => {
       console.log(`Server is running on ${PORT}`)
     })
--- cli.js
+++ cli.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
 
-const { SELECT_ALL } = require('./commands.sql')
+const { SELECT_ALL, CREATE_BLOGS } = require('./commands.sql')
 
 const sequelize = new Sequelize(process.env.FLY_POSTGRE_URL)
 
 const main = async () => {
   try {
     await sequelize.authenticate()
+    await sequelize.query(CREATE_BLOGS, { type: QueryTypes.CREATE })
     console.log('Connection establihed!!')
     const notes = await sequelize.query(SELECT_ALL, { type: QueryTypes.SELECT })
     notes.forEach((note) => {
