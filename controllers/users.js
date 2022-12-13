5e9c099dd315478a1a9dd3588c13244b607d1e09
utkuzun <utkuzun50@gmail.com>
2022-12-13 20:46:49 +0300

    put endpoint added

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const { CustomApiError } = require('../error/CustomApiError')
 
 const { User } = require('../models/User')
 
 const router = express.Router()
+
+const userFinder = async (req, res, next) => {
+  const user = await User.findOne({
+    where: { username: req.params.username },
+  })
+
+  if (!user) {
+    throw new CustomApiError('User can not be found!!', 404)
+  }
+
+  req.user = user
+  next()
+}
 
 router.get('/', async (req, res) => {
   const users = await User.findAll()
@@ -12,6 +26,16 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
   const newUser = await User.create(req.body)
   return res.status(201).json(newUser.toJSON())
+})
+
+router.put('/:username', userFinder, async (req, res) => {
+  await User.update(req.body, {
+    where: {
+      username: req.user.username,
+    },
+  })
+
+  return res.status(200).end()
 })
 
 module.exports = router
