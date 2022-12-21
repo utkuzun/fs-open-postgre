df747589db8305c88e01e5c994c104cb9884c59f
utkuzun <utkuzun50@gmail.com>
2022-12-21 15:58:05 +0300

    reading list adding authorization

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,5 +1,5 @@
 const express = require('express')
-// const { CustomApiError } = require('../error/CustomApiError')
+const { CustomApiError } = require('../error/CustomApiError')
 
 const { UserNotes } = require('../models')
 const { authenticate } = require('../utils/middleware')
@@ -7,6 +7,11 @@ const { authenticate } = require('../utils/middleware')
 const router = express.Router()
 
 router.post('/', authenticate, async (req, res) => {
+  console.log(req.user.userId, req.body.userId)
+  if (req.user.userId !== Number(req.body.userId)) {
+    throw new CustomApiError('Unauthorized!!', 403)
+  }
+
   const readinglist = await UserNotes.create(req.body)
   return res.status(201).json(readinglist.toJSON())
 })
